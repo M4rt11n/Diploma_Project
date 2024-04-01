09741ebec187933fe42574941c4c566972087c68
M4rt11n <marti030405@gmail.com>
2024-04-01 16:06:26 +0300

    db to be added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./Styles/App.scss";
-import Button from "./Elements/Button/Button";
 import "./Styles/Button.scss";
 import NavBar from "./Components/Navbar";
 import "./Styles/NavBar.scss";
 import "./Styles/Footer.scss";
-import SongTitle from "./Components/SongTitle";
-import Text from "./Elements/Text";
-import Title from "./Elements/Title";
-import Link from "./Elements/Link";
-import Pic from "./Elements/Pic";
 import SongPage from "./Pages/SongPage/SongPage";
 import "./Styles/SongPage.scss";
 import SongList from "./Pages/SongList/SongList";
